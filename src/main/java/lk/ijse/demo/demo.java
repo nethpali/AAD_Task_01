2138f653c8bd8caa5e08d1c14d229122cf3abd92
Nethpali_Jayaweera <nethpalijayaweera@gmail.com>
2024-02-03 10:59:47 +0530

    first commit

---
+++ src/main/java/lk/ijse/demo/demo.java
@@ -0,0 +1,26 @@
+package lk.ijse.demo;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class demo extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        if (req.getContentType() == null || !req.getContentType().toLowerCase().startsWith("application/json")) {
+            resp.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
+        }else {
+            var writer = resp.getWriter();
+            resp.setContentType("text/html");
+
+            System.out.println("Hello " + name);
+            writer.println("Hello " + name);
+ }
+}
+
+
+}
